1645b5aa7cc47f0e1b93895a89b09ebf1cb5f04a
TheEpicBlock <git.teb@theepicblock.nl>
2021-10-14 22:09:07 +0200

    Add items

--- src/main/java/nl/theepicblock/melonarmor/MelonArmor.java
+++ src/main/java/nl/theepicblock/melonarmor/MelonArmor.java
@@ -1,21 +1,33 @@
 package nl.theepicblock.melonarmor;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MelonArmor implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final String MODID = "melon-armor";
+
+	public static final MelonMaterial MELON_MATERIAL = new MelonMaterial();
+	public static final ArmorItem MELON_HELMET = new ArmorItem(MELON_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.COMBAT));
+	public static final ArmorItem MELON_CHESTPLATE = new ArmorItem(MELON_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(ItemGroup.COMBAT));
+	public static final ArmorItem MELON_LEGGINGS = new ArmorItem(MELON_MATERIAL, EquipmentSlot.LEGS, new Item.Settings().group(ItemGroup.COMBAT));
+	public static final ArmorItem MELON_BOOTS = new ArmorItem(MELON_MATERIAL, EquipmentSlot.FEET, new Item.Settings().group(ItemGroup.COMBAT));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		Registry.register(Registry.ITEM, id("melon_helmet"), MELON_HELMET);
+		Registry.register(Registry.ITEM, id("melon_chestplate"), MELON_CHESTPLATE);
+		Registry.register(Registry.ITEM, id("melon_leggings"), MELON_LEGGINGS);
+		Registry.register(Registry.ITEM, id("melon_boots"), MELON_BOOTS);
+	}
 
-		LOGGER.info("Hello Fabric world!");
+	private static Identifier id(String path) {
+		return new Identifier(MODID, path);
 	}
 }
---
+++ src/main/java/nl/theepicblock/melonarmor/MelonMaterial.java
@@ -0,0 +1,53 @@
+package nl.theepicblock.melonarmor;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ArmorMaterials;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class MelonMaterial implements ArmorMaterial {
+    private static final int[] DURABILITY = new int[]{13/2, 15/2, 16/2, 11/2};
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return DURABILITY[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return ArmorMaterials.NETHERITE.getProtectionAmount(slot);
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 1;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ENTITY_SLIME_SQUISH;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.MELON_SLICE);
+    }
+
+    @Override
+    public String getName() {
+        return "melon";
+    }
+
+    @Override
+    public float getToughness() {
+        return 2.0F;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0.3F;
+    }
+}
